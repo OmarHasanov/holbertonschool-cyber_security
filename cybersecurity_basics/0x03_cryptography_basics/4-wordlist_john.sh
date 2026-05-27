3e08a18d3d526a15b91b8ce685f5394efb5bb151
OmarHasanov <omerhesenov1998@gmail.com>
2026-05-27 23:57:08 +0400

    Cryptography Basics

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
-echo '#!/bin/bash' > 4-wordlist_john.sh
-echo 'john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=raw-sha256 > /dev/null; john --show "$1" --format=raw-sha256 | cut -d ":" -f 2 > 4-password.txt' >> 4-wordlist_john.sh
+#!/bin/bash
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" --format=raw-sha256 > /dev/null; john --show "$1" --format=raw-sha256 | cut -d ":" -f 2 > 4-password.txt
